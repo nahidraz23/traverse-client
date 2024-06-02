886e1c12c471ba9c67b2633acc76ebf15f92d2b8
nahidraz23 <nzamanraz@gmail.com>
2024-06-02 22:09:37 +0600

    Theme controll implemented

---
+++ src/Routes/router.jsx
@@ -0,0 +1,21 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layouts/MainLayout";
+import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
+    }
+])
