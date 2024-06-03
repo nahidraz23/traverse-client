6de9d709bf4a0c95657e8ca4494cc806297ad60d
nahidraz23 <nzamanraz@gmail.com>
2024-06-03 15:44:58 +0600

    Youtube video player coverted into a component

---
+++ src/Components/Shared/YoutubeVideo.jsx
@@ -0,0 +1,14 @@
+const YoutubeVideo = ({ src }) => {
+    return (
+        <iframe className='video rounded-2xl'
+            title='Youtube player'
+            sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
+            src={`https://youtube.com/embed/${src}?autoplay=0`}
+            width={"667px"}
+            height={"375px"}
+        >
+        </iframe>
+    );
+};
+
+export default YoutubeVideo;
--- src/Pages/Home/TourGuideSection/TourGuideSection.jsx
+++ src/Pages/Home/TourGuideSection/TourGuideSection.jsx
@@ -1,9 +1,9 @@
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import './TourGuideSection.css'
-import { useState } from 'react';
+import YoutubeVideo from '../../../Components/Shared/YoutubeVideo';
 
 const TourGuideSection = () => {
-    const [youtubeID] = useState('rn0nkEZdoRg')
+    const youtubeID = 'rn0nkEZdoRg';
 
     return (
         <div className='container mx-auto'>
@@ -22,14 +22,9 @@ const TourGuideSection = () => {
                     <TabPanel>
                         <div className='flex justify-center'>
                             <div className="artboard artboard-horizontal phone-2">
-                                <iframe className='video rounded-2xl'
-                                    title='Youtube player'
-                                    sandbox='allow-same-origin allow-forms allow-popups allow-scripts allow-presentation'
-                                    src={`https://youtube.com/embed/${youtubeID}?autoplay=0`}
-                                    width={"667px"}
-                                    height={"375px"}
-                                    >
-                                </iframe>
+                                <YoutubeVideo
+                                    src={youtubeID}
+                                ></YoutubeVideo>
                             </div>
                         </div>
                     </TabPanel>
